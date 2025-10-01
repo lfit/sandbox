33ada1e0b77fe9aa51ef8fdce0950c64a1136b30
modesevenindustrialsolutions <mwatkins@linuxfoundation.org>
2025-10-01 11:02:26 +0000

    CHORE: bump net.logstash.logback:logstash-logback-encoder from 7.4 to 8.1
    ## Release notes
    
    Sourced from net.logstash.logback:logstash-logback-encoder's releases.
    
    logstash-logback-encoder-8.1
    
    What's Changed
    New features and improvements
    
    Update LoggingEventJsonPatternParser to fix breaking logback change by @​jordanjennings in #1061
    
    Bug fixes
    
    Always build source jar by @​philsttr in logfellow/logstash-logback-encoder#1037
    
    Documentation, Tests and Build
    
    Bump copyright year to 2025 by @​philsttr in logfellow/logstash-logback-encoder#1064
    
    Dependency Upgrades
    
    Bump org.jacoco:jacoco-maven-plugin from 0.8.10 to 0.8.12 by @​dependabot in logfellow/logstash-logback-encoder#1031
    Bump org.apache.maven.plugins:maven-source-plugin from 3.3.0 to 3.3.1 by @​dependabot in logfellow/logstash-logback-encoder#1034
    Bump org.apache.maven.plugins:maven-gpg-plugin from 3.0.1 to 3.2.4 by @​dependabot in logfellow/logstash-logback-encoder#1033
    Bump org.sonatype.plugins:nexus-staging-maven-plugin from 1.6.13 to 1.7.0 by @​dependabot in logfellow/logstash-logback-encoder#1030
    Bump actions/upload-artifact@v4 by @​philsttr in logfellow/logstash-logback-encoder#1062
    Bump dependencies to latest by @​philsttr in logfellow/logstash-logback-encoder#1063
    Bump maven to 3.9.9 by @​philsttr in logfellow/logstash-logback-encoder#1065
    
    New Contributors
    
    @​jordanjennings made their first contribution in #1061
    
    Full Changelog: https://github.com/logfellow/logstash-logback-encoder/compare/logstash-logback-encoder-8.0...logstash-logback-encoder-8.1
    logstash-logback-encoder-8.0
    This release supports the new logback-access 2.x artifacts at group id ch.qos.logback.access. This release does not support the old logback-access artifact at ch.qos.logback:logback-access. Special thanks to @​sondemar for this contribution!
    This release requires Java 11 or newer.
    Recommended versions of dependencies are as follows (Earlier versions might work, but have not been tested):
    
    logback-core/classic 1.5.x.
    logback-access 2.0.x
    jackson-* 2.17.x
    
    What's Changed
    New features and improvements
    
    Set AbstractThrowableClassNameJsonProvider.getThrowable as protected by @​EvaristeGalois11 in logfellow/logstash-logback-encoder#988
    Add composite MdcEntryWriter to filter MDC keys with regex by @​jug in logfellow/logstash-logback-encoder#974
    
    Bug fixes
    
    Fix typo in IllegalArgumentException message by @​rooday-doordash in logfellow/logstash-logback-encoder#1024
    
    Documentation, Tests and Build
    
    Add jdk21 to build matrix by @​philsttr in logfellow/logstash-logback-encoder#1027
    Update readme for 8.0 by @​philsttr in logfellow/logstash-logback-encoder#1029
    
    Dependency Upgrades
    
    Support for logback-access 2.0.0 by @​sondemar in logfellow/logstash-logback-encoder#1015
    Upgrade dependencies and maven plugins by @​philsttr in logfellow/logstash-logback-encoder#1028
    
    New Contributors
    
    ... (truncated)
    
    ## Commits
    
    a998591 [maven-release-plugin] prepare release logstash-logback-encoder-8.1
    16714f1 [release]
    88c5d27 [release]
    ed1b470 Update readme for 8.1
    f280cfd Bump maven to 3.9.9 (#1065)
    053afde Bump copyright year to 2025 (#1064)
    a03fd11 Bump dependencies to latest (#1063)
    f4e5ff3 Update LoggingEventJsonPatternParser to fix breaking logback change (#1061)
    83aa0fe Bump actions/upload-artifact@v4 (#1062)
    76f5295 Bump org.sonatype.plugins:nexus-staging-maven-plugin (#1030)
    Additional commits viewable in compare view
    
    ![Dependabot compatibility score](https://docs.github.com/en/github/managing-security-vulnerabilities/about-dependabot-security-updates#about-compatibility-scores)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: modesevenindustrialsolutions <mwatkins@linuxfoundation.org>
    Change-Id: I02c3944319f73a1dec08a38680ffe13f68f2e9f9
    GitHub-PR: https://github.com/onap/portal-ng-bff/pull/37
    GitHub-Hash: 4308cefa1c972047

---
+++ app/src/test/java/org/onap/portalng/bff/actions/GetActionsIntegrationTest.java
@@ -0,0 +1,77 @@
+/*
+ *
+ * Copyright (c) 2022. Deutsche Telekom AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ *
+ */
+
+package org.onap.portalng.bff.actions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.OffsetDateTime;
+import org.junit.jupiter.api.Test;
+import org.onap.portalng.bff.openapi.client_history.model.ActionsListResponseHistoryDto;
+import org.onap.portalng.bff.openapi.server.model.ActionsListResponseApiDto;
+
+class GetActionsIntegrationTest extends ActionsMocks {
+
+  @Test
+  void thatActionCanBeRetrievedWithParameterShowLastHours() throws Exception {
+    int numberOfActions = 10;
+    Integer showLastHours = 2;
+    String userId = "22-33-44-55";
+    OffsetDateTime createdAt = OffsetDateTime.now();
+    ActionsListResponseHistoryDto actionsListResponseHistoryDto =
+        ActionFixtures.generateActionsListResponse(numberOfActions, 30, createdAt);
+
+    mockGetActions(actionsListResponseHistoryDto, userId, showLastHours);
+
+    final ActionsListResponseApiDto response = getActions(userId);
+
+    assertThat(response.getTotalCount()).isEqualTo(30);
+    assertThat(response.getItems()).hasSize(numberOfActions);
+    assertThat(response.getItems().get(0).getActionCreatedAt())
+        .isEqualTo(actionsListResponseHistoryDto.getActionsList().get(0).getActionCreatedAt());
+    assertThat(objectMapper.writeValueAsString(response.getItems().get(0).getAction()))
+        .isEqualTo(
+            objectMapper.writeValueAsString(
+                actionsListResponseHistoryDto.getActionsList().get(0).getAction()));
+  }
+
+  @Test
+  void thatActionCanBeRetrievedWithoutParameterShowLastHours() throws Exception {
+    int numberOfActions = 10;
+    String userId = "22-33-44-55";
+    OffsetDateTime createdAt = OffsetDateTime.now();
+    ActionsListResponseHistoryDto actionsListResponseHistoryDto =
+        ActionFixtures.generateActionsListResponse(numberOfActions, 30, createdAt);
+
+    mockGetActionsWithoutParameterShowLastHours(actionsListResponseHistoryDto, userId);
+
+    final ActionsListResponseApiDto response = getActionsWithoutParameterShowLastHours(userId);
+
+    assertThat(response.getTotalCount()).isEqualTo(30);
+    assertThat(response.getItems()).hasSize(numberOfActions);
+    assertThat(response.getItems().get(0).getActionCreatedAt())
+        .isEqualTo(actionsListResponseHistoryDto.getActionsList().get(0).getActionCreatedAt());
+    assertThat(objectMapper.writeValueAsString(response.getItems().get(0).getAction()))
+        .isEqualTo(
+            objectMapper.writeValueAsString(
+                actionsListResponseHistoryDto.getActionsList().get(0).getAction()));
+  }
+}
